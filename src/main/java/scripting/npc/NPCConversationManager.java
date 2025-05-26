53d4c0160bff805a28aa328d229b662b56bd9053
Merogie_ <kiat_tom@hotmail.com>
2025-05-26 09:53:02 +0800

    Mute Gachapon Announcements

--- src/main/java/scripting/npc/NPCConversationManager.java
+++ src/main/java/scripting/npc/NPCConversationManager.java
@@ -448,7 +448,7 @@ public class NPCConversationManager extends AbstractPlayerInteraction {
         Gachapon.log(getPlayer(), item.getId(), map);
 
         if (item.getTier() > 2) { //Uncommon and Rare
-            Server.getInstance().broadcastMessage(c.getWorld(), PacketCreator.gachaponMessage(itemGained, map, getPlayer()));
+        //    Server.getInstance().broadcastMessage(c.getWorld(), PacketCreator.gachaponMessage(itemGained, map, getPlayer())); Merogie -- Muted Gachapon
         }
     }
 
