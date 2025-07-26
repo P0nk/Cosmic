4a795c4ab9f3e86a82ecd86df4ca96826b227b20
Merogie <kiat_tom@hotmail.com>
2025-07-26 08:31:27 +0800

    edits

--- src/main/java/constants/inventory/ItemConstants.java
+++ src/main/java/constants/inventory/ItemConstants.java
@@ -235,10 +235,12 @@ public final class ItemConstants {
 
     public static boolean isHair(int itemId) {
 //        return itemId >= 30000 && itemId < 37608;
-        String folderpath = "D://Devmaple/wz/Character.wz/Hair";
+        String folderpath = "C://Devmaple/wz/Character.wz/Hair";
         String fileName = "000" + itemId + ".img.xml";
         File file = new File(folderpath, fileName);
 //        System.out.println(file);
         return file.exists();
     }
 }
+
+
