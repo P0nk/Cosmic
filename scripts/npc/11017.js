8ad31f0490b47e319444dc1789da3258bdbc0b7b
Merogie_ <kiat_tom@hotmail.com>
2025-07-27 22:37:15 +0800

    changed npc diplayed for nxgacha

--- scripts/npc/11017.js
+++ scripts/npc/11017.js
@@ -4,7 +4,7 @@ function start() {
     const YamlConfig = Java.type('config.YamlConfig');
     if (YamlConfig.config.server.USE_ENABLE_CUSTOM_NPC_SCRIPT) {
         cm.dispose();
-        cm.openNpc(11016, "nxtgacha");
+        cm.openNpc(11017, "nxtgacha");
         return;
     }
 
