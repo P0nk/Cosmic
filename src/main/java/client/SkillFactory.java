bb0b9da861680e8ef044830efd100aeafa8a667d
Slimy <slimycs96@gmail.com>
2025-09-03 20:48:09 +0800

    Removed some printouts

--- src/main/java/client/SkillFactory.java
+++ src/main/java/client/SkillFactory.java
@@ -115,6 +115,7 @@ public class SkillFactory {
     }
 
     private static Skill loadFromData(int id, Data data) {
+        // System.out.println("SKILLID"+id);
         Skill ret = new Skill(id);
         boolean isBuff = false;
         int skillType = DataTool.getInt("skillType", data, -1);
