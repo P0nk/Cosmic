974f0af0834a8e4dfb68a3b06f74aa36822f994d
Merogie <kiat_tom@hotmail.com>
2026-01-03 14:51:35 +0800

    reverted hunger limited pet vac, further optimized pet vac

--- src/main/java/net/server/channel/handlers/PetLootHandler.java
+++ src/main/java/net/server/channel/handlers/PetLootHandler.java
@@ -1,23 +1,8 @@
 /*
-	This file is part of the OdinMS Maple Story Server
+    This file is part of the OdinMS Maple Story Server
     Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
-		       Matthias Butz <matze@odinms.de>
-		       Jan Christian Meyer <vimes@odinms.de>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation version 3 as published by
-    the Free Software Foundation. You may not use, modify or distribute
-    this program under any other version of the GNU Affero General Public
-    License.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+              Matthias Butz <matze@odinms.de>
+              Jan Christian Meyer <vimes@odinms.de>
 */
 package net.server.channel.handlers;
 
@@ -25,10 +10,8 @@ import client.Character;
 import client.Client;
 import client.inventory.InventoryType;
 import client.inventory.Pet;
-import config.YamlConfig;
 import net.AbstractPacketHandler;
 import net.packet.InPacket;
-import server.CashShop;
 import server.maps.MapItem;
 import server.maps.MapObject;
 import server.maps.MapObjectType;
@@ -38,8 +21,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
-import static java.sql.DriverManager.println;
-
 /**
  * @author TheRamon
  * @author Ronan
@@ -48,6 +29,7 @@ public final class PetLootHandler extends AbstractPacketHandler {
     @Override
     public final void handlePacket(InPacket p, Client c) {
         Character chr = c.getPlayer();
+        // [Debug removed]
 
         int petIndex = chr.getPetIndex(p.readInt());
         Pet pet = chr.getPet(petIndex);
@@ -59,27 +41,27 @@ public final class PetLootHandler extends AbstractPacketHandler {
         p.skip(13);
         int oid = p.readInt();
         MapObject ob = chr.getMap().getMapObject(oid);
-        try {
+
+        // ---------------------------------------------------------
+        // PART 1: Handle the specific item the pet clicked
+        // ---------------------------------------------------------
+        if (ob instanceof MapItem) {
             MapItem mapitem = (MapItem) ob;
+
+            // Meso Magnet Check
             if (mapitem.getMeso() > 0) {
                 if (!chr.isEquippedMesoMagnet()) {
                     c.sendPacket(PacketCreator.enableActions());
                     return;
                 }
-
-                if (chr.isEquippedPetItemIgnore()) {
-                    final Set<Integer> petIgnore = chr.getExcludedItems();
-                    if (!petIgnore.isEmpty() && petIgnore.contains(Integer.MAX_VALUE)) {
-                        c.sendPacket(PacketCreator.enableActions());
-                        return;
-                    }
-                }
-            } else {
+            }
+            // Item Pouch Check
+            else {
                 if (!chr.isEquippedItemPouch()) {
                     c.sendPacket(PacketCreator.enableActions());
                     return;
                 }
-
+                // Ignore List Check
                 if (chr.isEquippedPetItemIgnore()) {
                     final Set<Integer> petIgnore = chr.getExcludedItems();
                     if (!petIgnore.isEmpty() && petIgnore.contains(mapitem.getItem().getItemId())) {
@@ -88,49 +70,75 @@ public final class PetLootHandler extends AbstractPacketHandler {
                     }
                 }
             }
-            // --------- Added the below code ---------
-            // Get list of map item
-            int pethunger = pet.getFullness();
-            final double pickupRadius = (double) (500000 * pethunger) / 100;
-            boolean hasFreeEquip = chr.getInventory(InventoryType.EQUIP).getNumFreeSlot() > 0;
-            boolean hasFreeUse   = chr.getInventory(InventoryType.USE).getNumFreeSlot() > 0;
-            boolean hasFreeEtc   = chr.getInventory(InventoryType.ETC).getNumFreeSlot() > 0;
 
-            List<MapObject> items = c.getPlayer().getMap().getMapObjectsInRange(pet.getPos(), pickupRadius, Arrays.asList(MapObjectType.ITEM));
-            final Set<Integer> petIgnore = chr.getExcludedItems(); // get list of item ignore
-            if (!hasFreeEquip || !hasFreeUse || !hasFreeEtc) {
-                chr.showHint(
-                        "Pet can't loot: please free up some EQUIP, USE or ETC slots.",
-                        300
-                );
+            // Pickup Trigger Item
+            try {
+                chr.pickupItem(ob, petIndex);
+            } catch (Exception e) {
+                // If the main item is bugged, enable actions to unfreeze client
                 c.sendPacket(PacketCreator.enableActions());
-                return;
             }
-            for (MapObject item : items) { // loop thorugh map item
-                MapItem mapItem = (MapItem) item; // assign map item to mapItem variable
-                // Check loot details
+        } else {
+            // This handles the "Ghost Items" (items already vacced by previous packets)
+            c.sendPacket(PacketCreator.enableActions());
+            return;
+        }
+
+        // ---------------------------------------------------------
+        // PART 2: Smart Vac Loop
+        // ---------------------------------------------------------
+
+        // 1. Fail-fast Inventory Checks (Optimization)
+        boolean fullEtc = c.getPlayer().getInventory(InventoryType.ETC).getNumFreeSlot() < 1;
+        boolean fullEquip = c.getPlayer().getInventory(InventoryType.EQUIP).getNumFreeSlot() < 1;
+        boolean fullUse = c.getPlayer().getInventory(InventoryType.USE).getNumFreeSlot() < 1;
+
+        if (fullEtc && fullEquip && fullUse) {
+            chr.showHint("Pet vac stopped: All inventories are full.", 300);
+            return;
+        }
+
+        List<MapObject> items = c.getPlayer().getMap().getMapObjectsInRange(c.getPlayer().getPosition(), Double.POSITIVE_INFINITY, Arrays.asList(MapObjectType.ITEM));
+        final Set<Integer> petIgnore = chr.getExcludedItems();
+
+        for (MapObject item : items) {
+            if (!(item instanceof MapItem)) {
+                continue;
+            }
+
+            MapItem mapItem = (MapItem) item;
+
+            // We wrap individual pickup attempts in try-catch.
+            // If one specific item is bugged/null, we ignore it and continue looping.
+            try {
+                // 2. Smart Inventory Filter
+                if (mapItem.getMeso() > 0) {
+                    // Mesos always looted
+                } else {
+                    int itemId = mapItem.getItemId();
+                    int typePrefix = itemId / 1000000;
+
+                    // Skip specific item types if that inventory is full
+                    if (typePrefix == 1 && fullEquip) continue;
+                    else if (typePrefix == 2 && fullUse) continue;
+                    else if (typePrefix >= 4 && fullEtc) continue;
+
+                    if (petIgnore.contains(itemId)) continue;
+                }
+
+                // 3. Ownership & Quest Checks
                 boolean is_player_kill = mapItem.getOwnerId() == c.getPlayer().getId();
                 boolean is_party_kill = mapItem.getOwnerId() == c.getPlayer().getPartyId();
-                boolean common_or_meso_item = mapItem.getQuest() <= 0; // QuestID <=0 because mesos quest id is -1
+                boolean common_or_meso_item = mapItem.getQuest() <= 0;
                 boolean is_quest_item_and_active = c.getPlayer().getQuestStatus(mapItem.getQuest()) == 1;
+
                 if ((is_player_kill || is_party_kill) && (common_or_meso_item || is_quest_item_and_active)) {
-                    try {
-                        if (!petIgnore.contains(mapItem.getItemId())) { // !petIgnore.isEmpty() &&
-                            chr.pickupItem(mapItem, petIndex);
-//                            System.out.println("Looted!");
-//                            System.out.println();
-                        } else {
-                            c.sendPacket(PacketCreator.enableActions());
-                        }
-                    } catch (NullPointerException | ClassCastException e) {
-                        c.sendPacket(PacketCreator.enableActions());
-                    }
+                    chr.pickupItem(mapItem, petIndex);
                 }
-//                System.out.println("Looted!");
-//                System.out.println();
-            } // up to here;
-        } catch (NullPointerException | ClassCastException e) {
-            c.sendPacket(PacketCreator.enableActions());
+            } catch (Exception e) {
+                // Silently ignore buggy items to keep server running smooth
+                continue;
+            }
         }
     }
 }
