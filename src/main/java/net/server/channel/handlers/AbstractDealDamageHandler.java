e9113bb36ef0b51f3169428f4b9caad90d2a4373
Merogie <kiat_tom@hotmail.com>
2026-01-19 21:59:50 +0800

    increase range of warrior threshold

--- src/main/java/net/server/channel/handlers/AbstractDealDamageHandler.java
+++ src/main/java/net/server/channel/handlers/AbstractDealDamageHandler.java
@@ -204,15 +204,15 @@ public abstract class AbstractDealDamageHandler extends AbstractPacketHandler {
                     // We check squared distance to avoid expensive square root math.
                     // 2,500,000 = ~1580 pixels (Nearly 2x the width of the game screen).
                     double distSq = player.getPosition().distanceSq(monster.getPosition());
-                    double thresholdSq = 4500000.0;
+                    double thresholdSq = 8500000.0;
 
                     // 1. Job Buffers (Give ranged classes slightly more room)
                     if (attack.magic) {
-                        thresholdSq += 10000000.0; // Adds ~300px more range tolerance
+                        thresholdSq += 8500000.0; // Adds ~300px more range tolerance
                     }
 
                     if (attack.ranged) {
-                        thresholdSq += 10000000.0; // Adds ~300px more range tolerance
+                        thresholdSq += 8500000.0; // Adds ~300px more range tolerance
                     }
 
                     // 2. Skill Exemptions (Full Map Attacks & Long Range Snipes)
