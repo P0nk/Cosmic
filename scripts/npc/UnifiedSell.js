a87e56a178b96b15cdbf9c04540c550546ac346b
Merogie <kiat_tom@hotmail.com>
2026-02-06 08:02:34 +0800

    zipangu npcs

--- scripts/npc/UnifiedSell.js
+++ scripts/npc/UnifiedSell.js
@@ -22,75 +22,80 @@ function action(mode, type, selection) {
     if (status == 0) {
         var msg = "\t\t#e#bUnified Sell Assistant#k#n\r\n\r\n";
 
-        // Status & Toggles
-        // Use persistence methods exposed in NPCConversationManager
+        // Status & Toggles (Values needed for display)
         var autoSellOn = cm.isAutoSellEnabled();
         var sellUntradablesOn = cm.isSellUntradables();
         var sellRebirthsOn = cm.isSellRebirths();
 
-        // Aligned Toggles: State | Button
-        msg += "#L0#Auto-Sell: " + (autoSellOn ? "#g[ON]#k" : "#r[OFF]#k") + "#l\r\n";
-        msg += "#L1#Sell Untradables: " + (sellUntradablesOn ? "#g[ON]#k" : "#r[OFF]#k") + "#l\r\n";
-        msg += "#L2#Sell Rebirths: " + (sellRebirthsOn ? "#g[ON]#k" : "#r[OFF]#k") + "#l\r\n";
+        // 1. Feature Group (Moved to Top)
+        msg += "#L0#Sell From Slot (Slot Selection)#l\r\n";
+        msg += "#L1#Buyback Items#l\r\n";
+        msg += "#L2#Manage Locked Items#l\r\n";
 
         msg += "\r\n-------------------------\r\n";
+
+        // 2. Sell All Group (Middle)
         msg += "#L3#Sell All Equips#l\r\n";
         msg += "#L4#Sell All Use#l\r\n";
         msg += "#L5#Sell All Etc#l\r\n";
+
         msg += "\r\n-------------------------\r\n";
-        msg += "#L6#Sell From Slot (Slot Selection)#l\r\n";
-        msg += "#L7#Buyback Items#l\r\n";
-        msg += "#L8#Manage Locked Items#l\r\n";
+
+        // 3. Toggles (Moved to Bottom)
+        msg += "#L6#Auto-Sell: " + (autoSellOn ? "#g[ON]#k" : "#r[OFF]#k") + "#l\r\n";
+        msg += "#L7#Sell Untradables: " + (sellUntradablesOn ? "#g[ON]#k" : "#r[OFF]#k") + "#l\r\n";
+        msg += "#L8#Sell Rebirths: " + (sellRebirthsOn ? "#g[ON]#k" : "#r[OFF]#k") + "#l\r\n";
 
         cm.sendSimple(msg);
     } else if (status == 1) {
         if (selection == 0) {
-            cm.toggleAutoSell();
-            status = -1;
-            action(1, 0, 0); // Reload menu
+            // Sell From Slot (Old 6)
+            cm.dispose();
+            cm.openNpc(9000042);
         } else if (selection == 1) {
-            cm.toggleSellUntradables();
-            status = -1;
-            action(1, 0, 0);
+            // Buyback (Old 7)
+            cm.dispose();
+            cm.openNpc(9010000, "buyback");
         } else if (selection == 2) {
-            cm.toggleSellRebirths();
-            status = -1;
-            action(1, 0, 0);
+            // Manage Locked (Old 8)
+            cm.dispose();
+            cm.openNpc(9010000, "sellAllAssistant");
         } else if (selection == 3) {
-            // NPCConversationManager.sellAll now calls SellAllCommand.sellAllItems with PERSISTENT flags
-            // But wait, the API I added `sellAll(type, untradables, rebirths)` in previous step...
-            // Actually, I didn't change the API in NPCConversationManager to use defaults.
-            // Let me check my memory context... I added `toggleSellUntradables` etc.
-            // I did NOT change `sellAll` signature in NPCConversationManager.
-            // The existing `cm.sellAll` takes arguments.
-            // So here I must pass the persistent values explicitly.
+            // Sell All Equip (Old 3)
             var u = cm.isSellUntradables();
             var r = cm.isSellRebirths();
             var res = cm.sellAll("equip", u, r);
             cm.sendOk(res);
             cm.dispose();
         } else if (selection == 4) {
+            // Sell All Use (Old 4)
             var u = cm.isSellUntradables();
             var r = cm.isSellRebirths();
             var res = cm.sellAll("use", u, r);
             cm.sendOk(res);
             cm.dispose();
         } else if (selection == 5) {
+            // Sell All Etc (Old 5)
             var u = cm.isSellUntradables();
             var r = cm.isSellRebirths();
             var res = cm.sellAll("etc", u, r);
             cm.sendOk(res);
             cm.dispose();
         } else if (selection == 6) {
-            // Redirect to 9000042 (Original Sell From Slot NPC)
-            cm.dispose();
-            cm.openNpc(9000042);
+            // Auto Sell Toggle (Old 0)
+            cm.toggleAutoSell();
+            status = -1;
+            action(1, 0, 0); // Reload menu
         } else if (selection == 7) {
-            cm.dispose();
-            cm.openNpc(9010000, "buyback");
+            // Sell Untradables Toggle (Old 1)
+            cm.toggleSellUntradables();
+            status = -1;
+            action(1, 0, 0);
         } else if (selection == 8) {
-            cm.dispose();
-            cm.openNpc(9010000, "sellAllAssistant");
+            // Sell Rebirths Toggle (Old 2)
+            cm.toggleSellRebirths();
+            status = -1;
+            action(1, 0, 0);
         }
     }
 }
