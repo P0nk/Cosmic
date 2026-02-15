f06ea54f13888575ecbb07ddd27217d48109984d
Merogie <kiat_tom@hotmail.com>
2026-02-16 04:56:49 +0800

    push fix for leafre travel npc

--- scripts/npc/2082001.js
+++ scripts/npc/2082001.js
@@ -52,14 +52,10 @@ function action(mode, type, selection) {
         if (status == 0) {
             cm.sendYesNo("Do you want to go to Orbis? The trip takes #b5 minutes#k. The ship is boarding right now, would you like to go?");
         } else if (status == 1) {
-            if (cm.haveItem(4031331)) { // Ticket to Orbis/Leafre (same ID often or 4031045)
-                // Actually 2082001 usually takes the 4031331 too if it's the same system, 
-                // but standard Orbis-Leafre is 4031045.
-                // Looking at my previous edits, I used 4031331 for Ramini. 
-                // I'll stick to 4031331 for consistency since the user saw it work.
+            if (cm.haveItem(4031045)) { // Ticket to Orbis (Regular)
                 var em = cm.getEventManager("Cabin");
                 if (em.getProperty("entry") == "true") {
-                    cm.gainItem(4031331, -1);
+                    cm.gainItem(4031045, -1);
                     cm.warp(240000111); // Leafre Docked Map
                     cm.dispose();
                 } else {
@@ -67,7 +63,7 @@ function action(mode, type, selection) {
                     cm.dispose();
                 }
             } else {
-                cm.sendOk("Make sure you got a ticket to travel in this flight. Check your inventory.");
+                cm.sendOk("Make sure you got a #bTicket to Orbis (Regular)#k to travel in this flight. Check your inventory.");
                 cm.dispose();
             }
         }
