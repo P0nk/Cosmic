634d0123ac7a994422c75d1dcac57c9da1992093
Merogie <kiat_tom@hotmail.com>
2026-02-15 16:50:13 +0800

    leafre ride developed

--- scripts/event/Cabin.js
+++ scripts/event/Cabin.js
@@ -1,125 +1,111 @@
 /*
-    Boss Rush PQ (Refactored)
+    Orbis <-> Leafre Travel Script (Cabin)
 */
 
-var isPq = true;
-var minPlayers = 1, maxPlayers = 6;
-var minLevel = 1, maxLevel = 255;
-var entryMap = 970030100;
-var exitMap = 970030000;
-var recruitMap = 970030000;
-var clearMap = 970030000;
+// Polyfill: Map console.log to Java System.out
+var console = {
+    log: function (msg) {
+        var System = Java.type("java.lang.System");
+        System.out.println(msg);
+    },
+    error: function (msg) {
+        var System = Java.type("java.lang.System");
+        System.err.println(msg);
+    }
+};
 
-var minMapId = 970030001;
-var maxMapId = 970042711;
+// Maps
+var Orbis_Station;     // 200000131 (User Provided) // Station to verify ticket? Or is this just the map before docked?
+// Actually script logic usually moves from Docked -> Ride -> Destination Station
+// The NPC handles Station -> Docked
 
-var eventTime = 5;     // 5 minutes
-const maxLobbies = 7;
+var Orbis_Docked;      // 200000132
+var Leafre_Docked;     // 240000111
+
+var Ride_To_Leafre;    // 200090200
+var Ride_To_Orbis;     // 200090210
+
+var Orbis_Arrival;     // 200000100 (Standard)
+var Leafre_Arrival;    // 240000100 (Standard)
+
+// Time Settings
+var closeTime = 4 * 60 * 1000;    // Boarding closes at 4 mins
+var beginTime = 5 * 60 * 1000;    // Ride starts at 5 mins
+var rideTime = 5 * 60 * 1000;     // Ride lasts 5 mins (User Requested)
 
 function init() {
-    setEventRequirements();
-}
+    try {
+        closeTime = em.getTransportationTime(closeTime);
+        beginTime = em.getTransportationTime(beginTime);
+        rideTime = em.getTransportationTime(rideTime);
 
-function getMaxLobbies() {
-    return maxLobbies;
-}
+        var mf = em.getChannelServer().getMapFactory();
 
-function setEventRequirements() {
-    var reqStr = "";
-    reqStr += "\r\n    Number of players: ";
-    reqStr += (maxPlayers - minPlayers >= 1) ? minPlayers + " ~ " + maxPlayers : minPlayers;
-    reqStr += "\r\n    Level range: ";
-    reqStr += (maxLevel - minLevel >= 1) ? minLevel + " ~ " + maxLevel : minLevel;
-    reqStr += "\r\n    Time limit: " + eventTime + " minutes";
+        Orbis_Docked = mf.getMap(200000132);
+        Leafre_Docked = mf.getMap(240000111);
 
-    em.setProperty("party", reqStr);
-}
+        Ride_To_Leafre = mf.getMap(200090200);
+        Ride_To_Orbis = mf.getMap(200090210);
 
-function setEventExclusives(eim) {}
+        Orbis_Arrival = mf.getMap(200000100);
+        Leafre_Arrival = mf.getMap(240000100);
 
-function setEventRewards(eim) {
-    eim.setEventRewards(6, [3010061], [1]); // Trimmed for brevity, logic remains identical
-    // ... Add rest of reward logic from original ...
-}
-
-function getEligibleParty(party) {
-    var eligible = [];
-    var hasLeader = false;
-
-    if (party.size() > 0) {
-        var partyList = party.toArray();
-
-        for (var i = 0; i < party.size(); i++) {
-            var ch = partyList[i];
-            if (ch.getMapId() == recruitMap && ch.getLevel() >= minLevel && ch.getLevel() <= maxLevel) {
-                if (ch.isLeader()) hasLeader = true;
-                eligible.push(ch);
-            }
-        }
-    }
-
-    if (!(hasLeader && eligible.length >= minPlayers && eligible.length <= maxPlayers)) {
-        eligible = [];
-    }
-    return Java.to(eligible, Java.type('net.server.world.PartyCharacter[]'));
-}
-
-function setup(level, lobbyid) {
-    var eim = em.newInstance("BossRush" + lobbyid);
-    eim.setProperty("level", level);
-    eim.setProperty("lobby", lobbyid);
-
-    eim.startEventTimer(eventTime * 60000);
-    setEventRewards(eim);
-    setEventExclusives(eim);
-    return eim;
-}
-
-function playerEntry(eim, player) {
-    var map = eim.getMapInstance(entryMap + eim.getIntProperty("lobby"));
-    player.changeMap(map, map.getPortal(0));
-}
-
-function scheduledTimeout(eim) {
-    end(eim);
-}
-
-function playerExit(eim, player) {
-    eim.unregisterPlayer(player);
-    player.changeMap(exitMap, 0);
-}
-
-function end(eim) {
-    var party = eim.getPlayers();
-    for (var i = 0; i < party.size(); i++) {
-        playerExit(eim, party.get(i));
-    }
-    eim.dispose();
-}
-
-function clearPQ(eim) {
-    eim.stopEventTimer();
-    eim.setEventCleared();
-}
-
-// ---------- FILLER FUNCTIONS ----------
-function afterSetup(eim) {}
-function playerUnregistered(eim, player) {}
-function playerLeft(eim, player) { if (!eim.isEventCleared()) playerExit(eim, player); }
-function changedMap(eim, player, mapid) {
-    if (mapid < minMapId || mapid > maxMapId) {
-        if (eim.isEventTeamLackingNow(true, minPlayers, player)) { eim.unregisterPlayer(player); end(eim); } else { eim.unregisterPlayer(player); }
+        scheduleNew();
+        console.log("[Cabin JS] Travel initialized.");
+    } catch (e) {
+        console.error("[Cabin JS] Error in init: " + e);
     }
 }
-function changedLeader(eim, leader) { if (!eim.isEventCleared() && (leader.getMapId() < minMapId || leader.getMapId() > maxMapId)) end(eim); }
-function playerDead(eim, player) {}
-function playerRevive(eim, player) { if (eim.isEventTeamLackingNow(true, minPlayers, player)) { eim.unregisterPlayer(player); end(eim); } else { eim.unregisterPlayer(player); } }
-function playerDisconnected(eim, player) { if (eim.isEventTeamLackingNow(true, minPlayers, player)) { end(eim); } else { playerExit(eim, player); } }
-function leftParty(eim, player) { if (eim.isEventTeamLackingNow(false, minPlayers, player)) { end(eim); } else { playerLeft(eim, player); } }
-function disbandParty(eim) { if (!eim.isEventCleared()) end(eim); }
-function monsterValue(eim, mobId) { return 1; }
-function giveRandomEventReward(eim, player) { eim.giveEventReward(player); }
-function monsterKilled(mob, eim) {}
-function allMonstersDead(eim) {}
-function cancelSchedule() {}
-function dispose(eim) {}
+
+function scheduleNew() {
+    try {
+        em.setProperty("docked", "true");
+        em.setProperty("entry", "true");
+
+        Orbis_Docked.setDocked(true);
+        Leafre_Docked.setDocked(true);
+
+        em.schedule("stopEntry", closeTime);
+        em.schedule("takeoff", beginTime);
+    } catch (e) {
+        console.error("[Cabin JS] Error in scheduleNew: " + e);
+    }
+}
+
+function stopEntry() {
+    em.setProperty("entry", "false");
+    // Could spawn boxes or clear objects if needed
+}
+
+function takeoff() {
+    em.setProperty("docked", "false");
+
+    Orbis_Docked.setDocked(false);
+    Leafre_Docked.setDocked(false);
+
+    // Warp from Docked to Ride Maps
+    Orbis_Docked.warpEveryone(Ride_To_Leafre.getId());
+    Leafre_Docked.warpEveryone(Ride_To_Orbis.getId());
+
+    // Broadcast ship movement (optional visual)
+    Orbis_Docked.broadcastShip(false);
+    Leafre_Docked.broadcastShip(false);
+
+    em.schedule("arrived", rideTime);
+}
+
+function arrived() {
+    // Warp from Ride Maps to Destination Stations
+    Ride_To_Leafre.warpEveryone(Leafre_Arrival.getId(), 0);
+    Ride_To_Orbis.warpEveryone(Orbis_Arrival.getId(), 0);
+
+    // Broadcast ship arrival
+    Orbis_Docked.broadcastShip(true);
+    Leafre_Docked.broadcastShip(true);
+
+    scheduleNew();
+}
+
+// Required Filler Functions
+function cancelSchedule() { }
+function dispose() { }
