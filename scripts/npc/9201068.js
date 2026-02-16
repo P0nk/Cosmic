7923cf6d36679266410499610408b13dc77c7c10
Merogie <kiat_tom@hotmail.com>
2026-02-16 22:41:42 +0800

    train gate timing

--- scripts/npc/9201068.js
+++ scripts/npc/9201068.js
@@ -5,7 +5,27 @@ var em;
 
 function start() {
     em = cm.getEventManager("Subway");
-    var text = "Here's the ticket reader.";
+
+    // Time Logic
+    var cal = java.util.Calendar.getInstance();
+    var hour = cal.get(java.util.Calendar.HOUR_OF_DAY);
+    var min = cal.get(java.util.Calendar.MINUTE);
+    var nowStr = (hour < 10 ? "0" + hour : hour) + ":" + (min < 10 ? "0" + min : min);
+
+    // Wait Time Logic (10 min cycle: 0-5 Board, 5-10 Ride)
+    // Departure is at x5 minutes (5, 15, 25...)
+    var nowMs = java.lang.System.currentTimeMillis();
+    var cycle = nowMs % 600000;
+    var waitMs = 0;
+
+    if (cycle < 300000) { // Boarding Phase (0-5)
+        waitMs = 300000 - cycle;
+    } else { // Ride Phase (5-10) -> Next departure is next cycle's 5 min mark
+        waitMs = (600000 - cycle) + 300000;
+    }
+    var waitMin = Math.ceil(waitMs / 60000);
+
+    var text = "Here's the ticket reader.\r\nCurrent Time: #b" + nowStr + "#k\r\nThe next train leaves in: #b" + waitMin + " minutes#k";
     var hasTicket = false;
     if (cm.haveItem(4031713) && cm.getPlayer().getMapId() == 600010001) {
         text += "\r\n#b#L0##t4031713#";
