bfffbfc5f2175539e18828610d49584e1fdc8e59
Merogie <kiat_tom@hotmail.com>
2026-03-02 23:53:29 +0800

    ships timer

--- scripts/map/onUserEnter/101000301.js
+++ scripts/map/onUserEnter/101000301.js
@@ -8,11 +8,11 @@ function start(ms) {
     if (em.getProperty("docked") == "false") {
         ms.getClient().getPlayer().warpAhead(toMap);
     } else {
-        // Calculate time left for 10:00 - 12:00 boarding window (in 20m cycle)
+        // Calculate time left for 30:00 - 40:00 Orbis boarding window (in 60m cycle)
         var now = java.lang.System.currentTimeMillis();
-        var cycleTime = now % 1200000;
-        if (cycleTime >= 600000 && cycleTime < 720000) {
-            var timeLeft = 720000 - cycleTime;
+        var cycleTime = now % 3600000;
+        if (cycleTime >= 1800000 && cycleTime < 2400000) {
+            var timeLeft = 2400000 - cycleTime;
             var PacketCreator = Java.type("tools.PacketCreator");
             ms.getClient().sendPacket(PacketCreator.getClock(Math.floor(timeLeft / 1000)));
         }
